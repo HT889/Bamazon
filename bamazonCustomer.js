a4be5774bf2854ccee8b4fadc44365124909e586
Haig Talarian <haigtalarian@gmail.com>
2019-07-19 10:11:03 -0700

    another commit, still need to update product quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
- const mysql = require("mysql");
- const inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,80 +12,77 @@ var connection = mysql.createConnection({
 connectDB();
 
 function connectDB() {
-    connection.connect(async function (err) {
-        if (err) throw err;
-        console.log("connected as id" + connection.threadId + "\n");
+  connection.connect(async function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId + "\n");
 
-        await readProducts();
+    await readProducts();
 
-        var whatToBuy = await askWhatToBuy();
-        console.log(whatToBuy)
+    var whatToBuy = await askWhatToBuy();
+    console.log(whatToBuy)
 
-        await checkProductsQuantity(whatToBuy);
+    await checkProductsQuantity(whatToBuy);
 
 
-        connection.end()
+    connection.end()
 
-    })
+  })
 }
 
 async function readProducts() {
   return new Promise((resolve, reject) => {
-      console.log("Selecting all products...\n");
-      connection.query("SELECT * FROM products", function (err, res) {
-          if (err) reject(err);
-          for (i = 0; i < res.length; i++) {
-              console.log(
-                  res[i].id + " | Name: "
-                  + res[i].product_name + " | Department: "
-                  + res[i].department_name + " | Price: $"
-                  + res[i].price
-              );
-          }
-          resolve();
-      })
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) reject(err);
+      // console.log(res);
+      for (i = 0; i < res.length; i++) {
+        console.log(
+          `Item ID: ${res[i].item_id} | Item Name: ${res[i].product_name} | Department: ${res[i].department_name} | Quantity: ${res[i].stock_quantity} | Price: $${res[i].price}`)
+        }
+      resolve();
+    })
   })
 }
 
 
 async function checkProductsQuantity(x) {
   return new Promise((resolve, reject) => {
-  console.log("Checking all products quantity...\n");
-  connection.query("SELECT * FROM products WHERE id=?", [x.id], function(err, res){
+    console.log("Checking all products quantity...\n");
+    connection.query("SELECT * FROM products WHERE id=?", [x.id], function (err, res) {
       if (err) reject(err);
       console.log(res)
       console.log(parseInt(x.quantity))
-      if (res[0].stock_quantity > parseInt(x.quantity)){
-          console.log("You have purchased your item");
-      }else{
-          console.log("insufficient quantity!");
+      if (res[0].stock_quantity > parseInt(x.quantity)) {
+        console.log("You have purchased your item");
+      } else {
+        console.log("insufficient quantity!");
       }
+    })
+    resolve();
   })
-  resolve();
-  }) 
 }
 
 
 
 async function askWhatToBuy() {
   return new Promise((resolve, reject) => {
-      inquirer.prompt([
-          {
-              type: "input",
-              name: "id",
-              message: "What is the id of the item you would like to purchase?"
-          },
-          {
-              type: "input",
-              name: "quantity",
-              message: "How many would you like to purchase?"
-          }
-      ]).then(function (askResponse) {
-          resolve(askResponse);
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "What is the id of the item you would like to purchase?"
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to purchase?"
+      }
+    ]).then(function (askResponse) {
+      resolve(askResponse);
 
-      }).catch(function (err) {
-          reject(err);
-      })
+    }).catch(function (err) {
+      reject(err);
+    })
   });
   ;
 }
